79afdf40e6008c910b71665fe5a8256429f76d0e
TheIronYoan <yoan.cottrel2019@campus-eni.fr>
2020-04-15 18:08:57 +0200

    Création Sorties Basique

---
+++ src/Controller/EventController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Form\InsertEventType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/user/CreateEvent", name="CreateEvent")
+     */
+    public function createEvent(Request $request,EntityManagerInterface $em)
+    {
+        $event = new Event();
+        $eventForm = $this->createForm(InsertEventType::class,$event);
+        $eventForm->handleRequest($request);
+        if($eventForm->isSubmitted()){
+            $em->persist($event);
+            $em->flush();
+            //return $this->redirectToRoute("index");
+            return $this->redirectToRoute("CreateEvent");
+        }
+
+        return $this->render('user/createEvent.html.twig',[
+            "eventForm" =>$eventForm->createView()
+        ]);
+    }
+}
