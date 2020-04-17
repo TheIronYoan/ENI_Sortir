383d3a2df67faa66abd0c544bd5eafedda2f5b37
TheIronYoan <yoan.cottrel2019@campus-eni.fr>
2020-04-17 10:47:02 +0200

    Oups le eventController

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,31 +3,62 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\User;
 use App\Form\InsertEventType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class EventController extends AbstractController
 {
     /**
-     * @Route("/user/CreateEvent", name="CreateEvent")
+     * @Route("/event/CreateEvent", name="CreateEvent")
      */
     public function createEvent(Request $request,EntityManagerInterface $em)
     {
+        $dateIsGood=true;
         $event = new Event();
         $eventForm = $this->createForm(InsertEventType::class,$event);
         $eventForm->handleRequest($request);
         if($eventForm->isSubmitted()){
-            $em->persist($event);
-            $em->flush();
-            //return $this->redirectToRoute("index");
-            return $this->redirectToRoute("CreateEvent");
+            if($event->getSignInLimit()<=$event->getStart()){
+                $organizer=$em->getRepository(User::class)->find(3);
+                $event->setOrganizer($organizer);
+                $em->persist($event);
+                $em->flush();
+                //return $this->redirectToRoute("index");
+                return $this->redirectToRoute("CreateEvent");
+            }else{
+                $dateIsGood=false;
+            }
         }
 
         return $this->render('event/createEvent.html.twig',[
-            "eventForm" =>$eventForm->createView()
+            "eventForm" =>$eventForm->createView(),
+            "isDateGood" =>$dateIsGood
         ]);
+    }
+
+    /**
+     * @Route("/event/ListEvent", name="ListEvent")
+     */
+    public function listEvents(Request $request,EntityManagerInterface $em)
+    {
+        $idUser=3;
+        $eventRepo= $this->getDoctrine()->getRepository(Event::class);
+        $queryOrganized=$eventRepo->findBy(array('organizer'=>$idUser));
+        $dql="SELECT e FROM App\Entity\Event e";
+        $dql.=" WHERE e.organizer != :idUser";
+        $query = $em -> createQuery($dql);
+        $query->setParameter("idUser",$idUser);
+        $queryNotJoined = $query->getResult();
+
+        return $this->render("/event/listEvent.html.twig",[
+            "organized"=>$queryOrganized,
+            "notJoined"=>$queryNotJoined
+        ]);
+
     }
 }
