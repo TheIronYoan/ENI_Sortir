0053e2f5b48d1bb6f65d7d800c5b0e6571be8130
TheIronYoan <yoan.cottrel2019@campus-eni.fr>
2020-04-17 10:15:49 +0200

    Création Sortie et Liste Sortie

--- src/Form/InsertEventType.php
+++ src/Form/InsertEventType.php
@@ -4,27 +4,27 @@ namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\GreaterThan;
 
 class InsertEventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('start')
-            ->add('duration')
-            ->add('signInLimit')
-            ->add('maxUsers')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('start',DateTimeType::class)
+            ->add('duration', NumberType::class)
+            ->add('signInLimit',DateTimeType::class)
+            ->add('maxUsers', NumberType::class)
+            ->add('description', TextType::class)
         ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Event::class,
-        ]);
     }
 }
