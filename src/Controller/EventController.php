e00892513afa41433d0cdb2f3de04f2a98ff1c7d
TheIronYoan <yoan.cottrel2019@campus-eni.fr>
2020-04-20 10:02:10 +0200

    Ajout Edition Sortie et Utilise l'utilisateur Connecté

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -25,12 +25,12 @@ class EventController extends AbstractController
         $eventForm->handleRequest($request);
         if($eventForm->isSubmitted()){
             if($event->getSignInLimit()<=$event->getStart()){
-                $organizer=$em->getRepository(User::class)->find(3);
+                $organizer=$this->getUser();
                 $event->setOrganizer($organizer);
                 $em->persist($event);
                 $em->flush();
                 //return $this->redirectToRoute("index");
-                return $this->redirectToRoute("CreateEvent");
+                return $this->redirectToRoute("ListEvent");
             }else{
                 $dateIsGood=false;
             }
@@ -47,7 +47,7 @@ class EventController extends AbstractController
      */
     public function listEvents(Request $request,EntityManagerInterface $em)
     {
-        $idUser=3;
+        $idUser=$this->getUser()->getId();
         $eventRepo= $this->getDoctrine()->getRepository(Event::class);
         $queryOrganized=$eventRepo->findBy(array('organizer'=>$idUser));
         $dql="SELECT e FROM App\Entity\Event e ";
@@ -74,8 +74,8 @@ class EventController extends AbstractController
      */
     public function viewEvent(Request $request,EntityManagerInterface $em,$id)
     {
-        $user = new User();
-        $idUser=3;
+        $user = $this->getUser();
+        $idUser=$user->getId();
         $eventRepo= $this->getDoctrine()->getRepository(Event::class);
         $query=$eventRepo->findOneBy(['id'=>$id]);
         $eventForm = $this->createForm(EventRegisterType::class,$user);
@@ -93,14 +93,14 @@ class EventController extends AbstractController
         }
         if($eventForm->isSubmitted()){
             if(!$isJoined && !$isOrganizer){
-                $userRepo= $this->getDoctrine()->getRepository(User::class);
-                $user=$userRepo->findOneBy(['id'=>$idUser]);
+                //$userRepo= $this->getDoctrine()->getRepository(User::class);
+                //$user=$userRepo->findOneBy(['id'=>$idUser]);
                 $user->addEvent($query);
                 $em->persist($user);
                 $em->flush();
             }elseif ($isJoined){
-                $userRepo= $this->getDoctrine()->getRepository(User::class);
-                $user=$userRepo->findOneBy(['id'=>$idUser]);
+                //$userRepo= $this->getDoctrine()->getRepository(User::class);
+                //$user=$userRepo->findOneBy(['id'=>$idUser]);
                 $user->removeEvent($query);
                 $em->persist($user);
                 $em->flush();
@@ -115,6 +115,37 @@ class EventController extends AbstractController
         ]);
 
     }
+
+    /**
+     * @Route("/event/EditEvent/{id}", name="EditEvent")
+     */
+    public function editEvent(Request $request,EntityManagerInterface $em,$id)
+    {
+        $dateIsGood=true;
+        $eventRepo= $this->getDoctrine()->getRepository(Event::class);
+        $event=$eventRepo->findOneBy(['id'=>$id]);
+        if($event->getOrganizer()->getId()!=$this->getUser()->getId()){
+            return $this->redirectToRoute("ListEvent");
+        }
+        $eventForm = $this->createForm(InsertEventType::class,$event);
+        $eventForm->handleRequest($request);
+        if($eventForm->isSubmitted()){
+            if($event->getSignInLimit()<=$event->getStart()){
+                $em->persist($event);
+                $em->flush();
+                //return $this->redirectToRoute("index");
+                return $this->redirectToRoute("ListEvent");
+            }else{
+                $dateIsGood=false;
+            }
+        }
+
+        return $this->render('event/createEvent.html.twig',[
+            "eventForm" =>$eventForm->createView(),
+            "isDateGood" =>$dateIsGood
+        ]);
+    }
+
 
 
 
