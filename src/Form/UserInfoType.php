19e17426d8d087a3c35395a2ab94d835b0c774b9
TheIronMatthieu <61141922+TheIronMatthieu@users.noreply.github.com>
2020-04-21 09:53:44 +0200

    le formulaire pour showUser

---
+++ src/Form/UserInfoType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserInfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $message="";
+        $builder
+            ->add('name',null,['label'=> 'Nom' ])
+            ->add('firstname',null,['label'=> 'Prenom' ])
+            ->add('username',null,[
+                    'label'    => 'Pseudo',
+                    'attr' => ['class' => 'tinymce','pattern'=>'[a-Z]*','title'=>'Rentrer un champ ne contenant que .....']
+                ]
+            )
+            ->add('email',EmailType::class)
+            ->add('phone',null,['label'    => 'Téléphone'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
