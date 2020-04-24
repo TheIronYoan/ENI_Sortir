1a38354465097c13aa8f3042ba7c5d5390279580
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-04-24 07:46:12 +0200

    participant unique sur mail et username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  *
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("username",message="Ce pseudo est deha utilisé")
+ * @UniqueEntity("email",message="un compte existe deja pour ce mail")
  */
 class User implements UserInterface
 {
