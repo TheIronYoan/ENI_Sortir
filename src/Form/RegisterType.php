fd6c17e76458e38d6de5acf83253b03f1f5c6b6b
TheIronYoan <yoan.cottrel2019@campus-eni.fr>
2020-04-24 16:23:44 +0200

    BONUS

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -39,6 +41,11 @@ class RegisterType extends AbstractType
                 'label' => 'Photo de profil',
                 'mapped' => false,
                 'required' => false,
+            ])
+            ->add('campus', EntityType::class, [
+                'label'    => 'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'name',
             ])
         ;
     }
